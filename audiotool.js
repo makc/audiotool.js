b62080e6cd1ad8750888501b85c3151bdfb14123
makc <makc.the.great@gmail.com>
2016-01-09 06:39:30 +0100

    v0.1
    - smaller code
    - parse duration, in seconds

--- audiotool.js
+++ audiotool.js
@@ -13,18 +13,15 @@ function Audiotool () {
 		request (url, error, function (xhr) {
 			xhr.responseType = 'arraybuffer';
 			xhr.onreadystatechange = function () { if (xhr.readyState == 4) {
-				var bytes = new Uint8Array (xhr.response);
+				var i, bytes = new Uint8Array (xhr.response);
 
-				if (bytes[0] != 'P'.charCodeAt (0)) return error && error ();
-				if (bytes[1] != 'E'.charCodeAt (0)) return error && error ();
-				if (bytes[2] != 'A'.charCodeAt (0)) return error && error ();
-				if (bytes[3] != 'K'.charCodeAt (0)) return error && error ();
+				for (i = 0; i < 4; i++) if (bytes[i] != 'PEAK'.charCodeAt (i)) return error && error ();
 
 				var length = bytes.length;
 				if (length != bytes[8] * 256 + bytes[9] + 10) return error && error ();
 
 				var peaks = new Array (length - 10);
-				for (var i = 10; i < length; i++) peaks[i - 10] = bytes[i];
+				for (i = 10; i < length; i++) peaks[i - 10] = bytes[i];
 
 				success (peaks);
 			}};
@@ -36,35 +33,35 @@ function Audiotool () {
 
 	// 5am here, this better be working...
 	this.getRandomTrack = function (success, error) {
-		request ('https://api.audiotool.com/tracks/relevant/?limit=0', error, function (xhr) {
+		var base = 'https://api.audiotool.com/tracks/relevant/?limit=';
+		request (base + 0, error, function (xhr) {
 			xhr.onreadystatechange = function () { if (xhr.readyState == 4) {
-				var match = xhr.response.match (/<tracks offset="0" limit="0" count="(\d+)"/);
+				var match = xhr.response.match (/<tracks.+?count="(\d+)/);
 				if (match) {
 					var offset = (Math.random () * match[1]) | 0;
 
-					request ('https://api.audiotool.com/tracks/relevant/?limit=1&offset=' + offset, error, function (xhr) {
+					request (base + '1&offset=' + offset, error, function (xhr) {
 						xhr.onreadystatechange = function () { if (xhr.readyState == 4) {
-							var track = {
-								response: xhr.response.toString ()
-							};
+							var response = xhr.response + '', track = { response: response };
 
 							// parse important bits
 							track.urls = [
-								track.response.match (/<playbackURL mime-type="audio\/mpeg">([^<]+)/)[1],
-								track.response.match (/<playbackURL mime-type="application\/ogg">([^<]+)/)[1]
+								response.match (/<playbackURL.+?mpeg">([^<]+)/)[1],
+								response.match (/<playbackURL.+?ogg">([^<]+)/)[1]
 							];
 
-							track.bpm = parseFloat (track.response.match (/<bpm>([^<]+)/)[1]);
+							track.bpm = parseFloat (response.match (/<bpm>([^<]+)/)[1]);
+							track.duration = parseFloat (response.match (/<duration>([^<]+)/)[1]) * 1e-3;
 
 							// parse auxiliary bits
-							track.cover = track.response.match (/<coverURL width="512" height="512">([^<]+)/)[1];
+							track.cover = response.match (/<coverURL.+?512">([^<]+)/)[1];
 
-							var names = track.response.match (/<name>([^<]+)/g);
+							var names = response.match (/<name>([^<]+)/g);
 							track.name = names[0].match (/>([^<]+)/)[1];
 							track.author = names[1].match (/>([^<]+)/)[1];
 
 							// get them peaks
-							getPeaks (track.response.match (/<peaksURL mime-type="application\/octet-stream">([^<]+)/)[1], function (peaks) {
+							getPeaks (track.response.match (/<peaksURL.*?>([^<]+)/)[1], function (peaks) {
 								track.peaks = peaks; success (track);
 							}, error);
 						}};
